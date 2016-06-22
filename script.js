ceab06f5928b5bfa73d193f8030833c8efc3d0b9
Hanif Fakira <fakira.work@gmail.com>
2016-06-22 14:18:06 +0100

    fetching api from using user input

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ $(document).ready(function() {
       var username = input.val();
 
       console.log("username was: " + username);
+      getGithubInfo(username);
     }
   });
 });
+
+
+function getGithubInfo(username) {
+  var url = "https://api.github.com/users/" + username;
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", url, false);
+  xmlhttp.send();
+
+  var data = xmlhttp.responseText;
+
+  console.log(data);
+}
